0646e9b2d824295b6adc236d183b9ce298a65476
Kevin <knphillips01@gmail.com>
2020-09-20 15:33:54 -0500

    added some cell logic

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,5 +1,6 @@
 import React, {useState, useCallback, useRef} from 'react';
 import produce from 'immer';
+import operations from './Operations';
 
 const numRows = 30;
 const numCols = 30;
@@ -25,8 +26,34 @@ function Game() {
     if (!runningRef.current) {
       return;
     }
-    //simulate
-    setTimeout(runSimulation, 1000);
+    
+    setGrid((g) => {
+      return produce(g, gridCopy => {
+        for (let i  = 0; i < numRows; i++){
+          for (let j = 0; j < numCols; j++ ){
+            let neighbors = 0;
+            operations.forEach(([x, y]) => {
+              const newI = i + x;
+              const newJ = j + y;
+              
+              // checking the bounds of the grid to see if we went below/above
+              if(newI >= 0 && newI < numRows && newJ >= 0 && newJ < numCols){
+                //if have a live cell => add ones to the neighbors 
+                neighbors += g[newI][newJ]
+              }
+            })
+            // determines what happens to the cells 
+            if(neighbors < 2 || neighbors > 3){
+              gridCopy[i][j] = 0;
+            } else if(g[i][j] === 0 && neighbors === 3){
+              gridCopy[i][j] = 1; 
+            }
+         }
+        }
+      })
+    })
+
+   setTimeout(runSimulation, 1000);
   }, [])
   
   return (
@@ -43,20 +70,20 @@ function Game() {
         gridTemplateColumns: `repeat(${numCols}, 20px)`
       }}>
         {grid.map((rows, i) =>
-          rows.map((col, k) => (
+          rows.map((col, j) => (
             <div 
-            key={`${i}-${k}`}
+            key={`${i}-${j}`}
             onClick={() => {
               //set intial state for grid - if alive - set to dead
               const newGrid = produce(grid, gridCopy => {
-                gridCopy[i][k] = grid[i][k] ? 0 : 1;
+                gridCopy[i][j] = grid[i][j] ? 0 : 1;
               })
               setGrid(newGrid);
             }}
               style={{
                 width: 20,
                 height: 20,
-                backgroundColor: grid[i][k] ? "black" : undefined,
+                backgroundColor: grid[i][j] ? "black" : undefined,
                 border: 'solid 1px black'
               }}
           />
---
+++ src/components/Operations.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const operations = [
+    [0, 1],
+    [0, -1],
+    [1, -1],
+    [-1, 1],
+    [1, 1],
+    [-1, -1],
+    [1, 0],
+    [-1, 0]
+];
+
+export default operations;
